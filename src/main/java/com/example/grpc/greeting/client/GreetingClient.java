6ec0d6e9b145fc7e2e0870366747f63b849c52b7
farhad alam <farhad.alam@shohoz.com>
2020-04-07 03:15:23 +0600

    unary done

--- src/main/java/com/example/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/example/grpc/greeting/client/GreetingClient.java
@@ -1,6 +1,11 @@
 package com.example.grpc.greeting.client;
 
+import com.example.grpc.greeting.server.GreetServiceImpl;
 import com.proto.dummy.DummyServiceGrpc;
+import com.proto.greet.GreetRequest;
+import com.proto.greet.GreetResponse;
+import com.proto.greet.GreetServiceGrpc;
+import com.proto.greet.Greeting;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -12,7 +17,29 @@ public class GreetingClient {
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
                 .usePlaintext()
                 .build();
-        DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
+//        DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
+
+        // Created a greet service client blocking - sync
+        GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
+
+        // Created  a protocol buffer message
+        Greeting greeting = Greeting.newBuilder()
+                .setFirstName("Nishi")
+                .setLastName("Puchu")
+                .build();
+
+        // create rpc request
+
+        GreetRequest greetRequest = GreetRequest.newBuilder()
+                .setGreeting(greeting)
+                .build();
+
+        //  Call  a RPC and get back a GreetResponse
+        GreetResponse greetResponse = greetClient.greet(greetRequest);
+
+        // printout the result
+
+        System.out.println(greetResponse.getResult());
 
         System.out.println("Shutting down");
         channel.shutdown();
---
+++ src/main/java/com/example/grpc/greeting/server/GreetServiceImpl.java
@@ -0,0 +1,35 @@
+package com.example.grpc.greeting.server;
+
+import com.proto.greet.GreetRequest;
+import com.proto.greet.GreetResponse;
+import com.proto.greet.GreetServiceGrpc;
+import com.proto.greet.Greeting;
+import io.grpc.stub.StreamObserver;
+
+public class GreetServiceImpl  extends GreetServiceGrpc.GreetServiceImplBase {
+
+    @Override
+    public void greet(GreetRequest request, StreamObserver<GreetResponse> responseObserver) {
+//        super.greet(request, responseObserver);
+
+        // Extract the Fields we need
+        Greeting greeting = request.getGreeting();
+        String firstName = greeting.getFirstName();
+
+        // Create the response
+        String result = "Hello" + firstName;
+        GreetResponse response =  GreetResponse.newBuilder()
+                .setResult(result)
+                .build();
+
+        // send the response
+        responseObserver.onNext(response);
+
+        // complete the rpc call
+        responseObserver.onCompleted();
+
+
+
+    }
+
+}
